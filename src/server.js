b40deb97b4b1153fe6a23c92578e2ad73f97ea05
EldarMemetov <eldarm9347@gmail.com>
2025-05-04 00:03:03 +0200

    delete blog

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ import errorHandler from './middlewares/errorHandler.js';
 import { env } from './utils/env.js';
 import feedbackRoutes from './routers/feedbackRoutes.js';
 import reviewRoutes from './routers/reviewRoutes.js';
-import blogRoutes from './routers/blogRoutes.js';
 import logger from './middlewares/logger.js';
 import corsOptions from './utils/corsOptions.js';
 
@@ -20,7 +19,14 @@ export const setupServer = () => {
 
   app.use('/api/feedback', feedbackRoutes);
   app.use('/api/reviews', reviewRoutes);
-  app.use('/api/blogs', blogRoutes);
+
+  app.get('/', (req, res) => {
+    res.send({
+      message: 'Qvrix API is running 🚀',
+      status: 'OK',
+      timestamp: new Date().toISOString(),
+    });
+  });
 
   app.use(notFoundHandler);
   app.use(errorHandler);
