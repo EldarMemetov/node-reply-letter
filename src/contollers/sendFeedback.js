76dc803234bd493d712d7099f49c53d8e8bdebd6
EldarMemetov <eldarm9347@gmail.com>
2026-01-31 23:20:37 +0100

    + test sendFeedback

--- src/contollers/sendFeedback.js
+++ src/contollers/sendFeedback.js
@@ -10,24 +10,29 @@ export const sendFeedback = async (req, res, next) => {
     const { name, email, message, agree, lang } = req.body;
     if (!agree) throw createError(400, 'You must agree to the privacy policy');
 
-    res
-      .status(201)
-      .json({ message: '✅ Feedback received! We will reply shortly.' });
-
     try {
       await notifyAdmin('feedback', { name, email, message, lang });
     } catch (err) {
       console.error('🚨 notifyAdmin failed:', err);
-      return res
-        .status(500)
-        .json({ message: 'Failed to send email to admin', error: err.message });
+      return res.status(500).json({
+        message: 'Failed to send email to admin',
+        error: err.message,
+      });
     }
 
     try {
       await sendUserReply('feedback', { name, email, lang });
     } catch (err) {
       console.error('🚨 sendUserReply failed:', err);
+      return res.status(500).json({
+        message: 'Failed to send email to user',
+        error: err.message,
+      });
     }
+
+    res
+      .status(201)
+      .json({ message: '✅ Feedback received! We will reply shortly.' });
   } catch (err) {
     next(err);
   }
