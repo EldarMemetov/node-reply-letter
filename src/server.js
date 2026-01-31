92505737dc00d0b8fc32c43b51217e172deeb8de
EldarMemetov <eldarm9347@gmail.com>
2026-01-31 22:12:40 +0100

    add fix

--- src/server.js
+++ src/server.js
@@ -1,38 +1,3 @@
-// import express from 'express';
-// import cors from 'cors';
-// import notFoundHandler from './middlewares/notFoundHandler.js';
-// import errorHandler from './middlewares/errorHandler.js';
-// import { env } from './utils/env.js';
-// import feedbackRoutes from './routers/feedbackRoutes.js';
-// import reviewRoutes from './routers/reviewRoutes.js';
-// import logger from './middlewares/logger.js';
-// import corsOptions from './utils/corsOptions.js';
-
-// const PORT = Number(env('PORT', '3000'));
-
-// export const setupServer = () => {
-//   const app = express();
-
-//   app.use(logger);
-//   app.use(express.json());
-//   app.use(cors(corsOptions));
-
-//   app.use('/api/feedback', feedbackRoutes);
-//   app.use('/api/reviews', reviewRoutes);
-
-//   app.get('/', (req, res) => {
-//     res.send({
-//       message: 'Qvrix API is running 🚀',
-//       status: 'OK',
-//       timestamp: new Date().toISOString(),
-//     });
-//   });
-
-//   app.use(notFoundHandler);
-//   app.use(errorHandler);
-
-//   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-// };
 import express from 'express';
 import cors from 'cors';
 import notFoundHandler from './middlewares/notFoundHandler.js';
