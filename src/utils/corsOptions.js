67799e564847dcf718c6b8ab135c2c8617be8d6d
EldarMemetov <eldarm9347@gmail.com>
2026-02-01 09:28:59 +0100

    fix corsOptions

--- src/utils/corsOptions.js
+++ src/utils/corsOptions.js
@@ -8,15 +8,12 @@ const allowedOrigins = [
 
 const corsOptions = {
   origin: (origin, callback) => {
-    console.log('CORS origin:', origin);
-    if (allowedOrigins.includes(origin)) {
+    if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error(`CORS blocked origin: ${origin}`));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
 
